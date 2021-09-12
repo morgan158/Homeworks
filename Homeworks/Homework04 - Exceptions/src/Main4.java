d88fe07e71d0042b6f1ffd569824bcd1cabd145c
Svetlana Astapova <morgan159@mail.ru>
2021-09-12 19:40:24 +0500

    add class User, modified class UserAuthorization (add code in override methods)

--- Homeworks/Homework04 - Exceptions/src/Main4.java
+++ Homeworks/Homework04 - Exceptions/src/Main4.java
@@ -1,5 +1,8 @@
 public class Main4 {
     public static void main(String[] args) {
+        UserAuthorization userAuthorization = new UserAuthorization();
+        userAuthorization.signUp("sdd@ss.ru", "ssdd44wwww");
+        System.out.println(userAuthorization.users);
 
     }
 }
--- Homeworks/Homework04 - Exceptions/src/UserAuthorization.java
+++ Homeworks/Homework04 - Exceptions/src/UserAuthorization.java
@@ -1,2 +1,28 @@
-public class UserAuthorization {
+import java.util.ArrayList;
+
+public class UserAuthorization implements UsersService {
+    ArrayList<User> users = new ArrayList<User>();
+
+    @Override
+    public void signUp(String email, String password) {
+        if (email.contains("@") && password.length() > 7) {
+            User user = new User();
+            user.setEmail(email);
+            user.setPassword(password);
+            users.add(user);
+            System.out.println("Пользователь успешно зарегистрирован");
+        }
+    }
+
+    @Override
+    public void signIn(String email, String password) {
+        for (User user : users) {
+            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
+                System.out.println("Пользователь успешно авторизован");
+                break;
+            } else {
+                System.out.println("Пользователя нет в базе данных");
+            }
+        }
+    }
 }
