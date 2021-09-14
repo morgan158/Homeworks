be392ec6eeeaf388964c89aea4b26c7577147fea
Svetlana Astapova <morgan159@mail.ru>
2021-09-15 02:29:40 +0500

    redo the projects struсture, add module homework05, add class Main5

---
+++ Homeworks/src/Homework04 - Exceptions/Main4.java
@@ -0,0 +1,9 @@
+public class Main4 {
+    public static void main(String[] args) {
+        UserAuthorization userAuthorization = new UserAuthorization();
+        userAuthorization.signUp("dsss@sss.rss", "4s333ds44");
+        userAuthorization.signUp("andrei@mail.ru", "qwery8yy");
+        userAuthorization.signIn("andrei@mail.ru", "qwerty123");
+
+    }
+}
--- Homeworks/src/Homework05 - UserRepository/Main5.java
+++ Homeworks/src/Homework05 - UserRepository/Main5.java
@@ -1,8 +1,8 @@
-public class Main4 {
+public class Main5 {
     public static void main(String[] args) {
         UserAuthorization userAuthorization = new UserAuthorization();
         userAuthorization.signUp("dsss@sss.rss", "4s333ds44");
-        userAuthorization.signUp("andrei@mail.ru", "qwerty123");
+        userAuthorization.signUp("andrei@.mail.ru", "qwerty123");
         userAuthorization.signIn("andrei@mail.ru", "qwerty123");
 
     }
